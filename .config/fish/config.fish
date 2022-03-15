b3a36e3e88d20445981f62c7fd9a47989389ff4e
Gabriel Guerra <gfranklings@gmail.com>
2022-03-15 12:27:23 -0400

    added void logo to the fish shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -24,7 +24,29 @@ set fish_cursor_visual block
 set fish_color_quote brgreen
 
 # prompt
-starship init fish | source
+# starship init fish | source
 
 # startup
 ~/.config/color-scripts/launch.sh
+
+# functions
+function launch_cs ()
+  set color_script $argv[1]
+  if test color_script != ""
+    if not test -d $HOME/.config/color-scripts
+      echo "launch_cs: $HOME/.config/color-scripts does not exists"
+    end
+    if test -f $HOME/.config/color-scripts/$color_script
+      $HOME/.config/color-scripts/$color_script
+    else
+      echo "launch_cs: $color_script not found"
+    end
+  end
+end
+
+function get_cs ()
+  if not test -d $HOME/.config/color-scripts then
+    echo "get_cs: $HOME/.config/color-scripts does not exists"
+  end
+  ls -la $HOME/.config/color-scripts
+end
